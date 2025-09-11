162ecf2af51e544c923ca97d0a942b37e1921c87
MrM-81 <masolakabelo@gmail.com>
2025-09-12 00:33:46 +0200

    First commit, inital setup

---
+++ src/main/java/nrs/NumberRangeSummarizerApplication.java
@@ -0,0 +1,13 @@
+package nrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberRangeSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberRangeSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nrs/NumberRangeSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package nrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberRangeSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
